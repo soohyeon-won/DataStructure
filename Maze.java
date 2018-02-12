dc72a3e8499a1828809e3a63cac1db3b2b8bbec9
soohyeon-won <30689094+soohyeon-won@users.noreply.github.com>
2018-02-12 09:04:51 +0000

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -14,7 +14,9 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 /**
-* google java coding style guide* <미로에서 가능한 모든 경로 찾기> 
+* google java coding style guide
+* Maze 클래스 설명
+* <미로에서 가능한 모든 경로 찾기> 
 * 주어진 미로의 시작 위치에서 탈출 위치까지 여러 개의 경로가 존재할 때  
 * 사이클을 형성하지 않는 가능한 모든 경로를 찾는다. 
 * 주어진 파일의 첫째 줄은 사각형으로 주어진 미로의 행과 열의 수 
@@ -26,21 +28,19 @@ import java.util.Scanner;
 */
 public class Maze {
 
-	//필드
-	private String[][] maze;	// 미로
-	private int row;	//행
-	private int col;	//열
-	private int[] start = new int[2]; //스타트 지점을 저장하는 배열
-	private int[] exit = new int[2];  //출구 지점을 저장하는 배열
+	private String[][] maze;		/* 미로 */
+	private int row;			/* 행 */
+	private int col;			/* 열 */
+	private int[] start = new int[2]; 	/* 스타트 지점을 저장하는 배열 */
+	private int[] exit = new int[2];  	/* 출구 지점을 저장하는 배열 */
 
-	int current_X;	//현재 위치 x좌표
-	int current_Y;	//현재 위치 y좌표
+	int current_X;				/* 현재 위치 x좌표 */
+	int current_Y;				/* 현재 위치 y좌표 */
 
-	private int distance;	//지나온 길의 개수.
-	private int paths=0;	//경로의 개수. (number of paths)
-	private ArrayList<String> coordinateOfPaths = new ArrayList<String>();	//지나온 길의 좌표들을 저장하는 list ex)(1,2)
+	private int distance;			/* 지나온 길의 개수. */
+	private int paths=0;			/* 경로의 개수. (number of paths) */
+	private ArrayList<String> coordinateOfPaths = new ArrayList<String>();	/* 지나온 길의 좌표들을 저장하는 list ex)(1,2) */
 
-	//메소드
 	/**
 	 * 미로에서 경로를 찾아 찾은 경로를 printMaze를 이용해 보여주고
 	 * 이동한 좌표와 이동횟수를출력해준다. (재귀 함수)
@@ -49,38 +49,33 @@ public class Maze {
 	 */
 	public void findMaze(int current_X, int current_Y){
 
-		//출구를 찾았을 때.
-		//경로의 개수를 저장.(path++)
-		//list에 있는 좌표들(지나온 길)을 출력해 준다.
+		/** 출구를 찾았을 때.
+		* 경로의 개수를 저장.(path++)
+		* list에 있는 좌표들(지나온 길)을 출력해 준다.
+		*/
 		if(!coordinateOfPaths.isEmpty()) {
 			if(current_X == exit[0] && current_Y == exit[1]){	//출구를 찾았을 때.
-				this.paths++;	//경로 개수 추가.
-				getDistance();	//이동 횟수와 이동 좌표들을 출력.
-				maze[exit[0]][exit[1]]="E"; //.으로 표시되어있을 출구를 E로 만들어준다.
-				printMaze();				//미로 출력
+				this.paths++;					//경로 개수 추가.
+				getDistance();					//이동 횟수와 이동 좌표들을 출력.
+				maze[exit[0]][exit[1]]="E"; 			//.으로 표시되어있을 출구를 E로 만들어준다.
+				printMaze();					//미로 출력
 			}
 		}
-
-		//시작위치가 아니라면 '.'값을 넣어준다.
-		if(!maze[current_X][current_Y].equals("S")){
-			maze[current_X][current_Y] = ".";
-			//지나간 좌표 저장
-			coordinateOfPaths.add("("+(current_X+1)+","+(current_Y+1)+")"+" ");
+	
+		if(!maze[current_X][current_Y].equals("S")){		 
+			maze[current_X][current_Y] = ".";		 /* 시작위치가 아니라면 '.'값을 넣어준다. */
+			coordinateOfPaths.add("("+(current_X+1)+","+(current_Y+1)+")"+" "); /* 지나간 좌표 저장 */
 		}
-		// 길이 있으면 이동하는 부분
-		//상
-		if(current_X>=1 && current_X < row && canMove(current_X-1, current_Y)){
-				findMaze(current_X-1, current_Y);
-		}
-		//하
-		if(current_X>=0 && current_X < row-1 && canMove(current_X+1, current_Y)){
+		/* 길이 있으면 이동하는 부분 */	
+		if(current_X>=1 && current_X < row && canMove(current_X-1, current_Y)){		
+				findMaze(current_X-1, current_Y); 	 	 	
+		}	
+		if(current_X>=0 && current_X < row-1 && canMove(current_X+1, current_Y)){ 	//하
 				findMaze(current_X+1, current_Y);
-		}
-		//좌
-		if(current_Y>=1 && current_Y<col && canMove(current_X, current_Y-1)){
+		} 
+		if(current_Y>=1 && current_Y<col && canMove(current_X, current_Y-1)){ 		//좌  					
 				findMaze(current_X, current_Y-1);
 		}
-		//우
 		if(current_Y>=0 && current_Y<col-1 && canMove(current_X, current_Y+1)){
 				findMaze(current_X, current_Y+1);
 		}
@@ -148,10 +143,9 @@ public class Maze {
 		Scanner in = new Scanner(file);
 		row = in.nextInt();
 		col = in.nextInt();
+		
 		maze = new String[row][col];
 		int i=0;
-		String line;
-		line = in.nextLine();
 		while(in.hasNextLine()){
 			line = in.nextLine();
 			this.maze[i]=line.split("");
@@ -190,14 +184,17 @@ public class Maze {
 		this.current_Y = start[1];
 	}
 
-	//경로의 개수를 출력하는 메소드
+	/**
+	*경로의 개수를 출력하는 메소드
+	*/	
 	public void getPath(){
 		System.out.println("number of Paths : "+ paths);
 		System.out.println();
 	}
 
-	//지나온 좌표들과 (coordinateOfPaths)
-	//와 이동횟수(distance)를 출력하는 메소드
+	/**
+	* 지나온 좌표들 (coordinateOfPaths), 이동횟수(distance)를 출력하는 메소드
+	*/
 	public void getDistance(){
 		distance = coordinateOfPaths.size();
 		System.out.print("Distance : "+ distance+"\t");	//이동횟수 출력
